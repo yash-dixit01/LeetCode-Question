5fcdd6ca3bc3665a97039da1fd8ebaee624e86b2
yash dixit <0251cs087@niet.co.in>
2026-06-24 22:00:10 +0530

    Time: 13 ms (42.15%), Space: 12.3 MB (89.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        str_x = str(x)
+        
+        return str_x == str_x[::-1]
