5b198209b0aedff0ad9c5f5250b477d587e52e14
yash dixit <0251cs087@niet.co.in>
2026-07-21 23:29:30 +0530

    Time: 4 ms (82.32%), Space: 46.5 MB (25.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,16 +3,21 @@ class Solution {
        if(s.length()!=t.length()){
             return false;
         }
-        int[] count = new int[26];
-        for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(count[i]!=0){
-                return false;
-            }
-        }
-        return true; 
+        // int[] count = new int[26];
+        // for(int i=0;i<s.length();i++){
+        //     count[s.charAt(i)-'a']++;
+        //     count[t.charAt(i)-'a']--;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(count[i]!=0){
+        //         return false;
+        //     }
+        // }
+        // return true; 
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        return Arrays.equals(s1, t1);
     }
 }
