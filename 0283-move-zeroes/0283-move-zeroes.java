6eabed5da346422f7408895f062f2cac2d901600
YASH DIXIT <0251cs087@niet.co.in>
2026-08-10 21:52:35 +0530

    Time: 1 ms (99.89%), Space: 47.8 MB (38.79%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] num) {
+      int n =num.length;
+      int j=0;
+      for(int i=0;i<n;i++){
+        if(num[i]!=0){
+            num[j]=num[i];
+            j++;
+        }
+      }  
+      for(int i=j;i<n;i++){
+        num[i]=0;
+      }
+    }
+}
