acd5fb24e8dfe0adcc92d2fa981663976abe00b3
YASH DIXIT <0251cs087@niet.co.in>
2026-08-11 20:54:41 +0530

    Time: 6 ms (35.63%), Space: 12.9 MB (66.59%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle:
+            return 0
+        dp = triangle[-1]
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+        return dp[0]
