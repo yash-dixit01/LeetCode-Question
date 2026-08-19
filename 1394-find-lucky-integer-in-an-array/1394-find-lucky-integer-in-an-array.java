1d6ec8b3a985bf8890e6e460a9edc3c0f7c69fc0
YASH DIXIT <0251cs087@niet.co.in>
2026-08-19 21:39:35 +0530

    Time: 1 ms (100%), Space: 45.2 MB (37.25%) - LeetHub

---
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int[] count = new int[501];
+        for (int num : arr) {
+            count[num]++;
+        }
+        for (int i = 500; i >= 1; i--) {
+            if (count[i] == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
